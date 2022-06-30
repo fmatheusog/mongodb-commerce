9cd4e9f2ff84767d8c45e70d604aee0f59174af0
Felipe Matheus <fmatheusog@gmail.com>
2022-06-30 15:07:57 -0300

    requisito 12: completo

---
+++ challenges/desafio12.js
@@ -0,0 +1,8 @@
+db.produtos.updateMany({
+  nome: { $ne: "McChicken" },
+  ingredientes: { $nin: ["ketchup"] },
+}, {
+  $push: { ingredientes: "ketchup" },
+});
+
+db.produtos.find({}, { _id: false, nome: true, ingredientes: true });
